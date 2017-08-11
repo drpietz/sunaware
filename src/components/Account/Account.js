1cd46b3015cb4408e4f607e8d06d084ed0538e68
Ben <5schmidtke@informatik.uni-hamburg.de>
2017-08-11 23:14:15 +0200

    repaired account.js

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -5,150 +5,150 @@ import PropTypes from 'prop-types'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
-import { login, register, logout} from '../../actions/auth'
+import { login, register, logout, updateProfile } from '../../actions/auth'
 
 class Account extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			username: null,
-			password: null,
-			displayname: null,
-			gps: true,
-			latitude: null,
-			longitude: null,
-			skinType: null
-		}
-	}
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: null,
+            password: null,
+            displayname: null,
+            gps: true,
+            latitude: null,
+            longitude: null,
+            skinType: null
+        }
+    }
 
-	handleInputChange = (event) => {
-		this.setState({ [event.target.name]: event.target.value })
-	}
+    handleInputChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    handleLogin = (event) => {
+        event.preventDefault()
+        this.props.actions.login(this.state.username, this.state.password)
+    }
+
+    handleUpdate = (event) => {
+        event.preventDefault()
+        this.props.actions.updateProfile(this.state.skinType, this.state.gps, this.state.latitude, this.state.longitude)
+    }
+
+    handleRegister = (event) => {
+        event.preventDefault()
+        this.props.actions.register(this.state.username, this.state.password, this.state.displayname)
+    }
+
+    handleLogout = (event) => {
+        this.props.actions.logout()
+    }
 
 
-	handleRegister = (event) => {
-		event.preventDefault()
-		this.props.actions.register(this.state.username, this.state.password, this.state.displayname)
-	}
-
-	handleLogout = (event) => {
-		this.props.actions.logout()
-	}
-
-
-	render() {
-		return (
+    render() {
+        return (
 			<div className='account'>
-				{ this.props.auth.isLoggedIn ? (
+                { this.props.auth.isLoggedIn ? (
 					<div className="column">
 						Welcome, { this.props.user.displayname }
 						<br/><br/>
 						<form onChange={this.handleInputChange}>
 							<p className="field">
-							<label className="label">Skin type</label>
-							<a className="control">
-								<a className="select" name="skinType">
-									<select>
-										<option>weiß</option>
-										<option>hell</option>
-										<option>medium</option>
-										<option>mediterran</option>
-										<option>braun</option>
-										<option>schwarz</option>
-									</select>
+								<label className="label">Skin type</label>
+								<a className="control">
+									<a className="select" name="skinType">
+										<select>
+											<option value={0}>weiß</option>
+											<option value={1}>hell</option>
+											<option value={2}>medium</option>
+											<option value={3}>mediterran</option>
+											<option value={4}>braun</option>
+											<option value={5}>schwarz</option>
+										</select>
+									</a>
 								</a>
-							</a>
-						</p>
-						<p className="field">
-							<a className="checkbox"> </a>
-							<label className="checkbox">
-								<input type="checkbox" name="gps" /> GPS enabled
-							</label>
-						</p>
-						<div className="field">
-							<label className="label">Longitude</label>
-								<input className="input" name="longitude" type="text" placeholder="Longitude input"/>
+							</p>
+							<p className="field">
+								<a className="checkbox"> </a>
+								<label className="checkbox">
+									<input type="checkbox" name="gps" /> GPS enabled
+								</label>
+							</p>
+							<div className="field">
+								<label className="label">Longitude</label>
+								<input className="input" name="longitude" type="number" step="any" placeholder="Longitude input"/>
 							</div>
 
 
-						<div className="field">
-							<label className="label">Latitude</label>
-								<input className="input" name="latitude" type="text" placeholder="Latitude input"/>
-						</div>
+							<div className="field">
+								<label className="label">Latitude</label>
+								<input className="input" name="latitude" type="number" step="any" placeholder="Latitude input"/>
+							</div>
 						</form>
 						<br/>
 						<p className="field">
 							<button className="button is-warning" onClick={this.handleLogout}>Logout</button>
+							<button className="button is-warning" onClick={this.handleUpdate}>Update</button>
 						</p>
 					</div>
-				) : (
-					<div className="hero is-fullheight bg-img">
-						<nav className="breadcrumb is-right is-small" aria-label="breadcrumbs">
-							<ul>
-								<li><Link to="/account">Login</Link></li>
-								<li><Link to="/start">Sign up</Link></li>
-								<li><a href="#">About us</a></li>
-							</ul>
-						</nav>
-						<form onChange={this.handleInputChange}>
-							<div className="field">
-								<label className="label">Name</label>
-								<div className="control has-icons-left has-icons-right">
-									<input className="input" name="displayname" type="text" placeholder="Name input" />
-									<span className="icon is-small is-left">
-										<i className="fa fa-user"/>
-									</span>
-								</div>
+                ) : (
+					<form onChange={this.handleInputChange}>
+						<div className="field">
+							<label className="label">Name</label>
+							<div className="control has-icons-left has-icons-right">
+								<input className="input" name="displayname" type="text" placeholder="Name input" />
+								<span className="icon is-small is-left">
+									<i className="fa fa-user"/>
+								</span>
 							</div>
+						</div>
 
-							<div className="field">
-								<label className="label">Email</label>
-								<div className="control has-icons-left">
-									<input className="input" name="username" type="text" placeholder="Email input" />
-									<span className="icon is-small is-left">
-										<i className="fa fa-envelope"/>
-									</span>
-								</div>
+						<div className="field">
+							<label className="label">Email</label>
+							<div className="control has-icons-left">
+								<input className="input" name="username" type="text" placeholder="Email input" />
+								<span className="icon is-small is-left">
+									<i className="fa fa-envelope"/>
+								</span>
 							</div>
+						</div>
 
-							<div className="field">
-								<label className="label">Password</label>
+						<div className="field">
+							<label className="label">Password</label>
 
-								<div className="control has-icons-left">
-									<input className="input" name="password" type="password" placeholder="Password"/>
-									<span className="icon is-small is-left">
-										<i className="fa fa-lock"/>
-									</span>
-								</div>
+							<div className="control has-icons-left">
+								<input className="input" name="password" type="password" placeholder="Password"/>
+								<span className="icon is-small is-left">
+									<i className="fa fa-lock"/>
+								</span>
 							</div>
+						</div>
 
-							<br />
-							<div>
-								<button className="button is-warning" onClick={this.handleLogin}>Login</button> <space/>
-								<button className="button is-warning" onClick={this.handleRegister}>Register</button>
-							</div>
+						<br />
+						<div>
+							<button className="button is-warning" onClick={this.handleLogin}>Login</button> <space/>
+							<button className="button is-warning" onClick={this.handleRegister}>Register</button>
+						</div>
 
-							<div className="fb-login-button" data-max-rows="1" data-size="medium" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="false"/>
+						<div className="fb-login-button" data-max-rows="1" data-size="medium" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="false"/>
 
-						</form>
-
-					</div>
-				)}
+					</form>
+                )}
 			</div>
-		)
-	}
+        )
+    }
 }
 
 Account.propTypes = {
-	user: PropTypes.object
+    user: PropTypes.object
 }
 
 function mapStateToProps(state) {
-	return { auth: state.auth, user: state.auth.user }
+    return { auth: state.auth, user: state.auth.user }
 }
 
 function mapDispatchToProps(dispatch) {
-	return { actions: bindActionCreators({ login, register, logout }, dispatch) }
+    return { actions: bindActionCreators({ login, register, logout, updateProfile}, dispatch) }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Account)
