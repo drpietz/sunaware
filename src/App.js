35c63a5cf03181487c8376f1fe7c729224e55472
chaosannie <5kunstma@informatik.uni-hamburg.de>
2017-08-11 16:06:55 +0200

    nix funktioniert mehr, hintergrund wird nicht als solcher erkannt, sondern zuerst dargstellt und danach die anderen komponenten

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css'
 import React, { Component } from 'react'
-import logo from './react_baqend.svg'
 
 import { Route, Switch } from 'react-router'
-import { BrowserRouter, NavLink } from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom'
 
 import { Provider } from 'react-redux'
 import createStore from './store/store'
+import { Link } from 'react-router-dom'
 
 import Account from './components/Account/Account'
 import Introduction from './components/Introduction/Introduction'
+import Start from './components/Start/Start'
 
 const store = createStore();
 
@@ -19,22 +20,26 @@ class App extends Component {
 			<Provider store={store}>
 				<BrowserRouter>
 					<div className="App">
-						<div style={{ backgroundColor: '#eee', padding: '16px' }}>
-							<img src={logo} style={{ height: '80px' }} alt="logo" />
-							<br />
-							<NavLink
-								style={{ padding: '0 8px' }}
-								activeStyle={{ textDecoration: 'underline' }}
-								to="/account"
-							>Account</NavLink>
+						<div className="hero is-fullheight bg-img">
+							<nav className="breadcrumb is-right is-small" aria-label="breadcrumbs">
+								<ul>
+									<li><Link to="/account">Login</Link></li>
+									<li><Link to="/start">Sign up</Link></li>
+									<li><a href="#">About us</a></li>
+								</ul>
+							</nav>
+						</div>J
+						<div className="App">
+							<Switch>
+								<Route exact path="/" component={Introduction}/>
+								<Route path="/account" component={Account}/>
+								<Route path="/start" component={Start}/>
+							</Switch>
 						</div>
-						<Switch>
-							<Route path="/account" component={Account}/>
-							<Route path="/" component={Introduction}/>
-						</Switch>
 					</div>
 				</BrowserRouter>
 			</Provider>
+
 		)
 	}
 }
