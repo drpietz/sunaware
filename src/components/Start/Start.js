59a06b21463fa08a86ba7c46ff41491086a7a399
Tim Pietz <t.pietz97@gmail.com>
2017-08-12 17:29:04 +0200

    Remove unused elements from start page

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -7,27 +7,18 @@ class Start extends Component {
 
 	render() {
 		return (
-
-			<div className="main">
-				<div className="hero is-fullheight bg-img">
-
-					<div className="hero-body">
-						<div className="container is-centered">
-							<h1 className="title">
-								<strong>Sunaware</strong>
-							</h1>
-							<iframe
-								width="1344"
-								height="600"
-								align="middle"
-								src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB-ZrFxLXdINcrzeYrlyg6H0QXIaDhJITY
-						&q=Space+Needle,Berlin">
-							</iframe>
-						</div>
-					</div>
-				</div>
+			<div className="is-centered">
+				<h1 className="title">
+					<strong>Sunaware</strong>
+				</h1>
+				<iframe
+					width="1344"
+					height="600"
+					align="middle"
+					src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB-ZrFxLXdINcrzeYrlyg6H0QXIaDhJITY
+			&q=Space+Needle,Berlin">
+				</iframe>
 			</div>
-
 		);
 	}
 }
