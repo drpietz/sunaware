3f95bb82b4cec8811d234b50e149b9d5126aa3a1
Tim Pietz <t.pietz97@gmail.com>
2017-08-19 14:23:45 +0200

    Display SunClock only on start page

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
+import SunClock from "../SunClock/SunClock";
 import UserReportsMap from '../UserReportsMap/UserReportsMap'
 import UserReportsFeed from '../UserReportsFeed/UserReportsFeed'
 import Content from "../Content/Content";
@@ -12,7 +13,8 @@ class Start extends Component {
 	render() {
 		return (
 			<PageBody>
-				<div style={{ width: '100%', height: 'calc(100vh - 3.25rem)', position: 'relative'}}>
+				<div style={{ width: '100%', height: 'calc(100vh - 3.25rem)', position: 'relative', overflow: 'hidden'}}>
+					<SunClock/>
 					<UserReportsMap style={{width: 'calc(100% - 5rem)'}} />
 					<UserReportsFeed style={{width: '5rem', height: '100%', position: 'absolute', top: '0', right: '0'}} />
 				</div>
--- src/components/SunawareLayout/SunawareLayout.js
+++ src/components/SunawareLayout/SunawareLayout.js
@@ -1,15 +1,12 @@
 import './SunawareLayout.css'
 import NavBar from '../NavBar/NavBar'
 import CloudFooter from '../CloudFooter/CloudFooter'
-import SunClock from '../SunClock/SunClock'
 
 import React from 'react'
 
 function SunawareLayout(props) {
 	return (
 		<section className="welcome-page hero is-fullheight">
-			<SunClock/>
-
 			<div className="hero-head">
 				<NavBar/>
 			</div>
