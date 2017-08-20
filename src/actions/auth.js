b6db258f1e71d22ab8c609f83406a3d48631fad4
Tim Pietz <t.pietz97@gmail.com>
2017-08-20 16:10:05 +0200

    Fix profile updates

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -39,24 +39,22 @@ export function updateProfile(fields) {
 	return {
 		'BAQEND': {
 			type: USER_PROFILE_UPDATE,
-			payload: (db) => {
-				let command = db.User.me.partialUpdate()
+			payload: (db) => db.User.me.load().then(user => {
 
 				// TODO: Nur Latitude ODER Longitude geändert => nur eins von beiden null, trotzdem update?
 				if (fields.latitude !== null && fields.longitude !== null) {
-					let position = new GeoPoint(parseFloat(fields.latitude), parseFloat(fields.longitude))
-					command.set('position', position)
+					user.position = new GeoPoint(parseFloat(fields.latitude), parseFloat(fields.longitude))
 				}
 
 				if (fields.positioningEnabled !== null) {
-					command.set('positioningEnabled', fields.positioningEnabled)
+					user.positioningEnabled = fields.positioningEnabled
 				}
 
 				if (fields.skinType !== null)
-					command.set('skinType', fields.skinType)
+					user.skinType = fields.skinType
 
-				return command.execute()
-			}
+				return user.save()
+			})
 		}
 	}
 }
