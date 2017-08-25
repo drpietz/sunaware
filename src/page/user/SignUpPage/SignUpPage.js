94bbf17029e709f386979b2b33ae9a8e2ca62e9e
Tim Pietz <t.pietz97@gmail.com>
2017-08-25 17:10:35 +0200

    Improve form validation on signup page

--- src/page/user/SignUpPage/SignUpPage.js
+++ src/page/user/SignUpPage/SignUpPage.js
@@ -17,69 +17,84 @@ class SignUp extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			displayname: null,
-			username: null,
-			password: null,
-            formErrors: {displayname: '', username: '', password: ''},
-            displaynameValid: false,
-            usernameValid: false,
-            passwordValid: false,
-            formValid: false
+			displayname: {
+				value: null,
+				getErrors: this.displaynameErrors,
+				showErrors: true
+			},
+			username: {
+				value: null,
+				getErrors: this.usernameErrors,
+				showErrors: true
+			},
+			password: {
+				value: null,
+				getErrors: this.passwordErrors,
+				showErrors: true
+			}
 		}
 	}
 
-    validateField(fieldName, value) {
-        let fieldValidationErrors = this.state.formErrors;
-        let displaynameValid = this.state.displaynameValid;
-        let usernameValid = this.state.usernameValid;
-        let passwordValid = this.state.passwordValid;
+	displaynameErrors = (state = this.state) => {
+		let value = this.state.displayname.value
 
-        switch(fieldName) {
-            case 'displayname':
-                displaynameValid = value.match(/^((\s*[a-z0-9]){3,20})$/i)
-                fieldValidationErrors.displayname = displaynameValid ? '' : 'not a valid name'
-                break;
-            case 'username':
-                usernameValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)
-                fieldValidationErrors.username = usernameValid ? '' : ' not an email address'
-                break;
-            case 'password':
-            	passwordValid = false
-                if(value.length < 3)
-                	fieldValidationErrors.password = 'password must be be at least 3 characters long'
-				else if (value.length >= 36)
-					fieldValidationErrors.password = 'password may not exceed 36 characters'
-                else {
-					passwordValid = true
-					fieldValidationErrors.password = ''
-				}
-				break
-            default:
-                break
-        }
-        this.setState({formErrors: fieldValidationErrors,
-			displaynameValid: displaynameValid,
-            usernameValid: usernameValid,
-            passwordValid: passwordValid
-        }, this.validateForm);
-    }
+		if (value === null)
+			return ['Name is required']
+		if (!value.match((/^((\s*[a-z0-9]){3,20})$/i)))
+			return ['Name is not valid']
+		else
+			return []
+	}
 
-    validateForm() {
-        this.setState({formValid: this.state.usernameValid && this.state.passwordValid && this.state.displaynameValid});
-    }
+	usernameErrors = (state = this.state) => {
+		let value = state.username.value
+
+		if (value === null)
+			return ['E-Mail is required']
+		if (!value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i))
+			return ['E-Mail is not valid']
+		else
+			return []
+	}
+
+	passwordErrors = (state = this.state) => {
+		let value = state.password.value
+
+		if (value === null)
+			return ['Password is required']
+		if (value.length < 3)
+			return ['Password is too short']
+		else if (value.length > 36)
+			return ['Password is too long']
+		else
+			return []
+	}
+
+	formErrors = (shownOnly = false, state = this.state) => {
+		const fields = ['displayname', 'username', 'password']
+
+		let errors = []
+		fields.forEach(field => {
+			if (!shownOnly || state[field].showErrors)
+				errors = [...errors, ...state[field].getErrors()]
+		})
+
+		return errors
+	}
 
     handleInputChange = event => {
 		event.preventDefault();
         const name = event.target.name;
         const value = event.target.value;
-        this.setState({[name]: value},
-            () => { this.validateField(name, value) });
+        this.setState({
+			[name]: {...this.state[name], value}
+		});
 	};
 
 	handleSignUp = event => {
 		event.preventDefault();
 
-		this.props.actions.register(this.state.username, this.state.password, this.state.displayname)
+		this.props.actions.register(this.state.username.value, this.state.password.value, this.state.displayname.value)
 	};
 
 	render () {
@@ -91,7 +106,7 @@ class SignUp extends Component {
 							<Field>
 								<Control hasIcons="left">
 									<Input name="displayname" placeholder="Displayname"
-										   isColor={this.props.errors && "danger"} />
+										   isColor={this.displaynameErrors().length > 0 && "danger"} />
 
 									<Icon isSize='small' isAlign='left'>
 										<i className="fa fa-user" />
@@ -102,7 +117,7 @@ class SignUp extends Component {
 							<Field>
 								<Control hasIcons="left">
 									<Input name="username" placeholder="E-Mail address"
-										   isColor={this.props.errors && "danger"} />
+										   isColor={this.usernameErrors().length > 0 && "danger"} />
 
 									<Icon isSize='small' isAlign='left'>
 										<i className="fa fa-envelope" />
@@ -112,7 +127,8 @@ class SignUp extends Component {
 
 							<Field>
 								<Control hasIcons="left">
-									<Input name="password" placeholder="Password" type="password" />
+									<Input name="password" placeholder="Password" type="password"
+										   isColor={this.passwordErrors().length > 0 && "danger"} />
 
 									<Icon isSize='small' isAlign='left'>
 										<i className="fa fa-lock" />
@@ -123,13 +139,13 @@ class SignUp extends Component {
 							<Field isGrouped="centered">
 								<Button isColor="warning"
 										isLoading={this.props.isPending}
-										disabled={!this.state.formValid}
+										disabled={this.formErrors().length > 0}
 										onClick={this.handleSignUp}>
 									Sign Up
 								</Button>
 							</Field>
 						</Box>
-						<FormErrors formErrors={this.state.formErrors} />
+						<FormErrors formErrors={this.formErrors(true)} />
 					</form>
 				</Content>
 			</PageBody>
